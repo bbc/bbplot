a3b8723b3aecdfa4fdb8e6c407c684217d5f65ed
jlopezper <jorge.loper@gmail.com>
2019-02-11 20:56:30 +0100

    adding font and offset arguments

--- R/bbc_style.R
+++ R/bbc_style.R
@@ -1,28 +1,46 @@
 #' Add bbc theme to ggplot chart
 #'
 #' This function allows you to add the bbc theme to your ggplotgraphics.
+#' @param font Font with which the plot is made. By default, the value is \emph{Helvetica} but Windows machines do not support this font type.
+#' @param offset Parameter that allows adding or substracting the \code{offset} value to the original font size. In particular, it modifies the size of:
+#' \itemize{
+#'   \item The chart's title
+#'   \item The chart's subtitle
+#'   \item The legend
+#'   \item The axis
+#'   \item Title of the facet-wrap
+#' }
 #' @keywords bbc_style
 #' @export
 #' @examples
+#' \dontrun{
 #' line <- ggplot(line_df, aes(x = year, y = lifeExp)) +
 #' geom_line(colour = "#007f7f", size = 1) +
 #' geom_hline(yintercept = 0, size = 1, colour="#333333") +
 #' bbc_style()
+#' }
+#'
+#'\dontrun{
+#' # Font changed to Arial and reduced by 5 points
+#' line <- ggplot(line_df, aes(x = year, y = lifeExp)) +
+#' geom_line(colour = "#007f7f", size = 1) +
+#' geom_hline(yintercept = 0, size = 1, colour="#333333") +
+#' bbc_style(font = "Arial", offset = -5)
+#' }
 
-bbc_style <- function() {
-  font <- "Helvetica"
+bbc_style <- function(font = "Helvetica", offset = 0) {
 
   ggplot2::theme(
 
   #Text format:
   #This sets the font, size, type and colour of text for the chart's title
   plot.title = ggplot2::element_text(family=font,
-                            size=28,
+                            size=28 + offset,
                             face="bold",
                             color="#222222"),
   #This sets the font, size, type and colour of text for the chart's subtitle, as well as setting a margin between the title and the subtitle
   plot.subtitle = ggplot2::element_text(family=font,
-                               size=22,
+                               size=22 + offset,
                                margin=ggplot2::margin(9,0,9,0)),
   plot.caption = ggplot2::element_blank(),
   #This leaves the caption text element empty, because it is set elsewhere in the finalise plot function
@@ -35,14 +53,14 @@ bbc_style <- function() {
   legend.title = ggplot2::element_blank(),
   legend.key = ggplot2::element_blank(),
   legend.text = ggplot2::element_text(family=font,
-                             size=18,
+                             size=18 + offset,
                              color="#222222"),
 
   #Axis format
   #This sets the text font, size and colour for the axis test, as well as setting the margins and removes lines and ticks. In some cases, axis lines and axis ticks are things we would want to have in the chart - the cookbook shows examples of how to do so.
   axis.title = ggplot2::element_blank(),
   axis.text = ggplot2::element_text(family=font,
-                           size=18,
+                           size=18 + offset,
                            color="#222222"),
   axis.text.x = ggplot2::element_text(margin=ggplot2::margin(5, b = 10)),
   axis.ticks = ggplot2::element_blank(),
@@ -58,8 +76,8 @@ bbc_style <- function() {
   #This sets the panel background as blank, removing the standard grey ggplot background colour from the plot
   panel.background = ggplot2::element_blank(),
 
-  #Strip background (#This sets the panel background for facet-wrapped plots to white, removing the standard grey ggplot background colour and sets the title size of the facet-wrap title to font size 22)
+  #Strip background (#This sets the panel background for facet-wrapped plots to white, removing the standard grey ggplot background colour and sets the title size of the facet-wrap title to font size 22 + offset)
   strip.background = ggplot2::element_rect(fill="white"),
-  strip.text = ggplot2::element_text(size  = 22,  hjust = 0)
+  strip.text = ggplot2::element_text(size  = 22 + offset,  hjust = 0)
   )
   }
